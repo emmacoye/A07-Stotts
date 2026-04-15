9f0d1af8948859ce2a6c7bd751bd3b3b8bc278ef
Emma Coye <ecoye@unc.edu>
2026-04-15 14:14:25 -0400

    Functioning collatz_length - passes all current tests

--- main.rs
+++ main.rs
@@ -3,6 +3,8 @@ fn main() {
     assert_eq!(collatz_length(1), 1, "length of 1 should be 1");
     assert_eq!(collatz_length(6), 9, "length of 6 should be 9");
     assert_eq!(collatz_length(27), 112, "length of 27 should be 112");
+    assert_eq!(collatz_length(2), 2);
+    assert_eq!(collatz_length(3), 8);
     println!("collatz_length checks passed.");
 
     let answer = longest_collatz(1_000_000);
@@ -13,7 +15,17 @@ fn main() {
 }
 
 fn collatz_length(n: u64) -> u64 {
-    todo!()
+    fn run(n: u64, steps: u64) -> u64 {
+        match n {
+            1 => steps,
+            _ => match n % 2 {
+                0 => run(n / 2, steps + 1),
+                _ => run(3 * n + 1, steps + 1),
+            },
+        }
+    }
+
+    run(n, 1)
 }
 
 fn longest_collatz(limit: u64) -> u64 {
